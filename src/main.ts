8cc1e37b65f12ff5263df5ca137bac16fa69fa93
AzizBelhadj1 <148291893+AzizBelhadj1@users.noreply.github.com>
2025-03-04 06:37:05 +0100

    1

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import * as $ from 'jquery';
-
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
