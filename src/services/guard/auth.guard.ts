45a869de6018fdee2c526ddc79851aabe350d646
Iyed519 <omri.iyed@esprit.tn>
2025-04-04 19:07:43 +0100

    new project

---
+++ src/services/guard/auth.guard.ts
@@ -0,0 +1,14 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {TokenService} from "../token/token.service";
+import {inject} from "@angular/core";
+import {KeycloakService} from "../keycloak/keycloak.service";
+
+export const authGuard: CanActivateFn = () => {
+  const  keycloakService: KeycloakService = inject(KeycloakService);
+  const router = inject(Router);
+  if (keycloakService.keycloak.isTokenExpired()) {
+    router.navigate(['login']);
+    return false;
+  }
+  return true;
+};
---
+++ src/services/interceptor/http-token.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor, HttpHeaders
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {TokenService} from "../token/token.service";
+import {Token} from "@angular/compiler";
+import {KeycloakService} from "../keycloak/keycloak.service";
+
+@Injectable()
+export class HttpTokenInterceptor implements HttpInterceptor {
+
+  constructor(
+    private keycloakService: KeycloakService
+  ) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token: string | undefined = this.keycloakService.keycloak.token;
+    if (token) {
+      const authReq: HttpRequest<any> = request.clone({
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        })
+      });
+      return next.handle(authReq);
+    }
+    return next.handle(request);
+  }
+}
