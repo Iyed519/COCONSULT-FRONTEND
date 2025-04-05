72c49edbceb32c73266c10d882a26fbb7b4c6def
Iyed519 <omri.iyed@esprit.tn>
2025-04-05 22:01:23 +0100

    new main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllTemplateBackComponent } from './components/backoffice/all-template-back/all-template-back.component';
 import { AllTemplateFrontComponent } from './components/frontoffice/all-template-front/all-template-front.component';
-import {authGuard} from "../services/guard/auth.guard";
-import {LoginComponent} from "./components/frontoffice/login/login.component";
+import { SigninComponent } from './components/frontoffice/signin/signin.component';
+import { AdminComponent } from './components/backoffice/admin/admin.component';
+import { LiabilityListComponent } from './components/backoffice/Liability/liability-list/liability-list.component';
+import { LiabilityFormComponent } from './components/backoffice/Liability/liability-form/liability-form.component';
+import { HomeComponent } from './components/backoffice/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: AllTemplateBackComponent },
-  { path: 'login', component: LoginComponent },
-  {path: 'dash' , component: AllTemplateBackComponent, canActivate: [authGuard] },
-  {path: 'horizontal' , component: AllTemplateBackComponent,canActivate: [authGuard]  },];
+   { path: '',
+    component:AllTemplateBackComponent,
+    children:[
+      { path:'aziz', component:HomeComponent },
+      { path: 'liability-list', component: LiabilityListComponent }
+    ]  },  
+   
+   { path: 'liability-form', component: LiabilityFormComponent }, // for creating
+   { path: 'liability-form/:id', component: LiabilityFormComponent },
+  { path: 'login', component: SigninComponent },
+{path: 'dash' , component: AllTemplateBackComponent  },
+{path: 'horizontal' , component: AllTemplateBackComponent  },
+{path: 'ad' , component: AdminComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
