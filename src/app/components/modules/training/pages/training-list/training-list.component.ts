cdec309040e64c3985aaf4acf17c7b66dabadbc8
Iyed519 <omri.iyed@esprit.tn>
2025-04-19 12:54:16 +0100

    enroll

--- src/app/components/modules/training/pages/training-list/training-list.component.ts
+++ src/app/components/modules/training/pages/training-list/training-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {TrainingService} from "../../../../../services/services/training.service";
 import {Router} from "@angular/router";
 import {PageResponseTrainingResponse} from "../../../../../services/models/page-response-training-response";
+import {TrainingResponse} from "../../../../../services/models/training-response";
 
 @Component({
   selector: 'app-training-list',
@@ -11,7 +12,9 @@ import {PageResponseTrainingResponse} from "../../../../../services/models/page-
 export class TrainingListComponent implements OnInit {
   trainingResponse: PageResponseTrainingResponse = {};
   page: number = 0 ;
-  size: number = 5;
+  size: number = 5  ;
+  // message: string = '';
+  level: string = '';
   constructor(
     private trainingService: TrainingService,
     private router: Router) {
@@ -32,4 +35,51 @@ export class TrainingListComponent implements OnInit {
       }
     });
   }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllTrainings();
+  }
+
+  goToPreviousPage() {
+    this.page--;
+    this.findAllTrainings()
+  }
+
+  goToPage(page: number) {
+    this.page = page;// page is index (l index ta3 html)
+    this.findAllTrainings();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllTrainings();
+  }
+
+  goToLastPage() {
+    this.page = this.trainingResponse.totalPages as number -1;
+    this.findAllTrainings();
+  }
+  get isLastPage(): boolean {
+    return this.page == this.trainingResponse.totalPages as number -1;
+  }
+
+
+  //     (enroll)="enrollToTraining($event)" fel front
+  // enrollToTraining(training: TrainingResponse) {
+  //   this.message = ''
+  //   this.trainingService.enrollToTraining({
+  //     'training-id': training.id as number
+  //   }).subscribe({
+  //     next: () => {
+  //       this.level = 'success';
+  //       this.message = 'You have successfully enrolled to ' + training.name + 'training';
+  //     },
+  //      error (err):void => {
+  //       console.log(err);
+  //       this.level = 'error';
+  //       this.message = err.error.error;
+  //     }
+  //   })
+  // }
 }
